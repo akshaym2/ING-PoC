40e21727ccb9e27d1456ff54e523931003472f0c
Shashidhar Sr <shashidhar-s@HCL.COM>
2017-07-18 17:37:39 +0530

    Commit message

---
+++ employee.java
@@ -0,0 +1,9 @@
+PUSH CODE TO MASTER
+===================
+GIT bash here ---> git init(will create local repo) ---> git add .(Add the file to git repo) ---> git commit .-m "Commit message" ---> git remote add origin
+
+
+
+To Pull ===>
+git pull origin master --allow-unrelated-histories
+
