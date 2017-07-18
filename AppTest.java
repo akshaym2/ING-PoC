e53d425736b006c743ccddd244c8fedc2f04b159
Shashank Srivastava <shashank.sr@HCL.COM>
2017-07-18 17:48:55 +0530

    commented by shashank

--- AppTest.java
+++ AppTest.java
@@ -35,4 +35,7 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+public String showMsg(){
+return "this App";
 }
--- hcl.java
+++ hcl.java
@@ -1,4 +1,5 @@
 class Demo{
+//put your coment
 public static void main(String...s){
 System.out.println("Hi, Welcome to ING GIT Training...!");
 }
